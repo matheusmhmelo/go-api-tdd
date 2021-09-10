a9aaaf86f018d290dbe1bc2d6669f801613e792f
matheusmelo <ma_hrm@hotmail.com>
2021-09-09 22:06:59 -0300

    write credentials

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
+const insertCredentialsSQL = "INSERT INTO credentials(receiver_id, organization_id, software_statement_id, client_id) VALUES ($1, $2, $3, $4)"
+
 type connector interface {
 	BeginFunc(ctx context.Context, f func(pgx.Tx) error) error
 }
@@ -18,4 +20,14 @@ func NewDatabase(conn connector) *Database {
 	return &Database{
 		conn: conn,
 	}
+}
+
+func (d *Database) WriteCredentials(ctx context.Context, receiverID, clientID, organizationID, ssID string) error {
+	return d.conn.BeginFunc(ctx, func(tx pgx.Tx) error {
+		_, err := tx.Exec(ctx, insertCredentialsSQL, receiverID, clientID, organizationID, ssID)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 }
--- internal/repository/repository_test.go
+++ internal/repository/repository_test.go
@@ -1,9 +1,13 @@
 package repository
 
 import (
+	"context"
+	"errors"
 	"reflect"
 	"testing"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"github.com/pashagolub/pgxmock"
 )
 
@@ -18,4 +22,71 @@ func Test_NewDatabase(t *testing.T) {
 	if !reflect.DeepEqual(*got, want) {
 		t.Errorf("got != want: %+v != %+v", got, want)
 	}
+}
+
+func TestDatabase_WriteCredentials(t *testing.T) {
+	mock, _ := pgxmock.NewConn()
+	db := NewDatabase(mock)
+
+	receiverID := "abc"
+	clientID := "123"
+	organizationID := "456"
+	ssID := "xyv"
+
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO credentials").
+		WithArgs(receiverID, clientID, organizationID, ssID).
+		WillReturnResult(pgxmock.NewResult("INSERT", 1))
+	mock.ExpectCommit()
+	mock.ExpectRollback()
+
+	err := db.WriteCredentials(context.Background(), receiverID, clientID, organizationID, ssID)
+	if err != nil {
+		t.Errorf("db.WriteCredentials got an error, expected nil: %s", err)
+	}
+}
+
+func TestDatabase_WriteCredentials_Errors(t *testing.T) {
+	t.Run("Unknown SQL error", func(t *testing.T) {
+		mock, _ := pgxmock.NewConn()
+		db := NewDatabase(mock)
+
+		receiverID := "abc"
+		clientID := "123"
+		organizationID := "456"
+		ssID := "xyv"
+
+		mock.ExpectBegin()
+		mock.ExpectExec("INSERT INTO credentials").
+			WithArgs(receiverID, clientID, organizationID, ssID).
+			WillReturnError(errors.New("unknown error"))
+		mock.ExpectCommit()
+		mock.ExpectRollback()
+
+		err := db.WriteCredentials(context.Background(), receiverID, clientID, organizationID, ssID)
+		if err == nil {
+			t.Error("db.WriteCredentials expected error, got nil")
+		}
+	})
+	t.Run("Credentials Already Exists", func(t *testing.T) {
+		mock, _ := pgxmock.NewConn()
+		db := NewDatabase(mock)
+
+		receiverID := "abc"
+		clientID := "123"
+		organizationID := "456"
+		ssID := "xyv"
+
+		mock.ExpectBegin()
+		mock.ExpectExec("INSERT INTO credentials").
+			WithArgs(receiverID, clientID, organizationID, ssID).
+			WillReturnError(&pgconn.PgError{Code: pgerrcode.UniqueViolation})
+		mock.ExpectCommit()
+		mock.ExpectRollback()
+
+		err := db.WriteCredentials(context.Background(), receiverID, clientID, organizationID, ssID)
+		if err == nil {
+			t.Error("db.WriteCredentials expected error, got nil")
+		}
+	})
 }
